4ac39f3523400d211c20f2e2124946122f9ba358
juanhiginio <juanhiginio06@gmail.com>
2025-11-20 01:09:34 -0500

    Implementación: Crear estructura del microservicio de autenticación

---
+++ microservicioGateway/src/test/java/com/juanhiginio/microservicioGateway/MicroservicioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanhiginio.microservicioGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
