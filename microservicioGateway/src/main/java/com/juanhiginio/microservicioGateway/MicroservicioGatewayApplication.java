c6ade4d9a10d481101e05366c6d6bd9c0fb1085f
juanhiginio <juanhiginio06@gmail.com>
2025-11-29 17:31:44 -0500

    Dependencias: Añadir dependencias y configuraciones necesarias para implementar gateway

--- microservicioGateway/src/main/java/com/juanhiginio/microservicioGateway/MicroservicioGatewayApplication.java
+++ microservicioGateway/src/main/java/com/juanhiginio/microservicioGateway/MicroservicioGatewayApplication.java
@@ -3,11 +3,19 @@ package com.juanhiginio.microservicioGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "API Gateway Documentación",
+        version = "1.0",
+        description = "Documentación para el Microservicio Gateway"
+))
 public class MicroservicioGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicioGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioGatewayApplication.class, args);
+    }
 
 }
